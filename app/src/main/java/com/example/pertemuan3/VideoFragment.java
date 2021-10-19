274fe46cf6536c511ceeb1ef2c55541da2a77a1e
Maggiejiiva <fadelardana08@gmail.com>
2021-10-19 23:32:18 +0700

    sekian commit

--- app/src/main/java/com/example/pertemuan3/VideoFragment.java
+++ app/src/main/java/com/example/pertemuan3/VideoFragment.java
@@ -9,10 +9,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class ChatFragment extends Fragment {
+public class VideoFragment extends Fragment {
     @Nullable
     @Override
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chat,container,false);
+
+        return inflater.inflate(R.layout.fragment_video,container,false);
     }
+
 }
